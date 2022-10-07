0312dada13eadad8b7071729103762a3dfb3a91f
Kuang Yu Heng <yuheng3107@gmail.com>
2022-10-07 20:35:09 +0800

    Added a basic UI for the game

--- logic.js
+++ logic.js
@@ -24,17 +24,34 @@ function playRound(playerSelection, computerSelection) {
     };
 
     if (playerSelection === beats[computerSelection]) {
+        computerScore++;
         return `You Lose! ${computerSelection[0].toUpperCase() + computerSelection.substring(1)} beats ${playerSelection[0].toUpperCase() + playerSelection.substring(1)}`;
     }
     if (computerSelection === beats[playerSelection]) {
+        playerScore++;
         return `You Win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection[0].toUpperCase() + computerSelection.substring(1)}`;
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Your move: ");
-        computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+// Set Scores
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    let outcome = playRound(e.target.textContent, getComputerChoice());
+    const div = document.createElement('div');
+    div.textContent = outcome;
+    const output = document.querySelector('#output');
+    output.appendChild(div);
+
+    const scores = document.querySelector('#scores');
+    scores.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+    
+
+    if (playerScore === 5) alert("You Win!");
+    if (computerScore === 5) alert("You Lose!");
+    
+
+}));
+
